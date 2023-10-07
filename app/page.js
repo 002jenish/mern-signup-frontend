57ae598c5f62eeebf6ea3ae77628f493e2089538
002jenish <jenishspacefire314@gmial.com>
2023-10-07 19:10:43 +0530

    frontend push

---
+++ app/page.js
@@ -0,0 +1,101 @@
+"use client";
+import React, { useState, useEffect } from "react";
+// import './App.css';
+
+export default function Home() {
+  const [form, setForm] = useState({});
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [usernames, setUsernames] = useState([]);
+
+  //<--------------------------------------------------------------------------------------------------->
+  //fetch data from the database
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/register");
+        if (response.ok) {
+          const data = await response.json();
+          setUsernames(data);
+        } else {
+          console.log("Failed to fetch usernames");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  });
+
+  //<--------------------------------------------------------------------------------------------------->
+  //to handle the from data
+  const handleForm = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  //<--------------------------------------------------------------------------------------------------->
+  //to handle the form default behaviour of refreshing
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/register", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+
+    if (response.ok) {
+      // Handle successful registration
+      alert("Registration successful");
+      /*setUsernames([...usernames, username]);
+      setUsername('');
+      setPassword('');*/ //
+    } else {
+      // Handle registration failure
+      alert("User Already exsit or Registrastion error");
+    }
+  };
+
+  //<--------------------------------------------------------------------------------------------------->
+  //main return code
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>username</label>
+        <input
+          type="text"
+          name="username"
+          value={form.username}
+          onChange={handleForm}
+          required
+        />
+        <label>password</label>
+        <input
+          type="text"
+          name="password"
+          value={form.password}
+          onChange={handleForm}
+          required
+        />
+        <input type="submit"></input>
+      </form>
+      {/* Display the list of usernames */}
+      <div>
+        <h4>Usernames:</h4>
+        <ul>
+          {usernames.map((user, index) => (
+            <li key={user._id}>{user.username}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
